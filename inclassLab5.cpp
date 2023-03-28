67681ef3813c5a50b31dcbdde0f9e37424a73bbb
Hithesh Jaywardana <83839061+hith3sh@users.noreply.github.com>
2023-03-28 22:34:06 +0530

    Update inclassLab5.cpp

--- inclassLab5.cpp
+++ inclassLab5.cpp
@@ -1,94 +1,68 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-void quickSort(int arr[], int left, int right)
-{
-   int i = left, j = right;
-   int pivot = arr[(left + right) / 2];
-   while (i <= j)
-   {
-      while (arr[i] < pivot)
-         i++;
-      while (arr[j] > pivot)
-         j--;
-      if (i <= j)
-      {
-         swap(arr[i], arr[j]);
-         i++;
-         j--;
-      }
-   }
-   if (left < j)
-      quickSort(arr, left, j);
-   if (i < right)
-      quickSort(arr, i, right);
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    
+    swap(arr[i+1], arr[high]);
+    return i+1;
+}
+//implements quicksort algorithm
+void quicksort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int p = partition(arr, low, high);
+        quicksort(arr, low, p-1);
+        quicksort(arr, p+1, high);
+    }
+}
+//this function will return the median of the array in float 
+float returnMedian(vector<int> arr) {
+   int size=arr.size();
+   sort(arr.begin(),arr.end());
+   if (size % 2 == 0) {
+      return (((float)arr[size/2]+(float)arr[size/2 -1])/2.0);
+    }
+    else {
+        return (float)arr[size/2];
+    }
 }
 
-float getMedian(int arr[], int size)
-{
-   sort(arr, arr + size);
-   if (size % 2 == 0)
-   {
-      return ((float)arr[size / 2 - 1] + (float)arr[size / 2]) / 2.0;
-   }
-   else
-   {
-      return (float)arr[size / 2];
-   }
-}
-
-int main()
-{
-   int size, num;
-   cout << "Enter the size of the array: ";
-   cin >> size;
-
-   // Dynamically allocate memory for array
-   int *arr = new int[size];
-
-   // Generate random numbers between 1 and 100 for each array element
+int main() {
+   int n,newNum;
+   //asking the user to enter the size of the array
+   cout<< "enter the size of the array: ";
+   cin >> n;
+   
+   //creating a vector array
+   vector<int> arr;
+   //generate random numbers from 0 to 99
    srand(time(0));
-   for (int i = 0; i < size; i++)
-   {
-      arr[i] = rand() % 100 + 1;
+   for(int i=0;i<n;i++){
+      arr.push_back(rand()%100);
    }
-
-   // Print the original array
-   cout << "Original array: ";
-   for (int i = 0; i < size; i++)
-   {
-      cout << arr[i] << " ";
+   //printing the existing array
+   cout << "existing array: ";
+   for(int i=0;i<n;i++){
+      cout << arr[i] <<" ";
    }
-   cout << endl;
-
-   // Ask user to add a number to the array
-   cout << "enter a new number to the array: ";
-   cin >> num;
-
-   // Add the user input number to the end of the array
-   arr[size] = num;
-   size++;
-
-   // Sort the array using quicksort
-   quickSort(arr, 0, size - 1);
-
-   // Print the sorted array
-   cout << "sorted array is: ";
-   for (int i = 0; i < size; i++)
-   {
-      cout << arr[i] << " ";
+   //asking user for a new number
+   cout <<endl << "enter a new number: ";
+   cin >> newNum;
+   arr.push_back(newNum); 
+   //printing the median value with sorted array
+   quicksort(arr,0,n);
+   cout<<"sorted array: ";
+   for(int i=0;i<arr.size();i++){
+      cout << arr[i] <<" ";
    }
-   cout << endl;
-
-   // Print the median of the sorted array
-   cout << "Median is: " << getMedian(arr, size) << endl;
-
-   // Free the dynamically allocated memory
-   delete[] arr;
-
+   cout <<"median is: " <<  returnMedian(arr);
    return 0;
 }
